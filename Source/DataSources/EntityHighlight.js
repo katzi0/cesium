f756b0a6d026abd23ff412642e5d6ec094e9d1a5
shai <shai@webiks.com>
2018-06-06 16:42:59 +0300

    added animation stop function

--- Source/DataSources/EntityHighlight.js
+++ Source/DataSources/EntityHighlight.js
@@ -15,11 +15,13 @@ define(['../DataSources/CallbackProperty','../Core/EasingFunction','../Core/defa
         this._easingFunction = defaultValue(props.easingFunction, 'ELASTIC_OUT');
         this._stopIncrease = false;
         this._scaleSum = 0;
+        this._scale = defaultValue(props.scale, this._minScale);
+        this._increase;
     }
 
     EntityHighlight.prototype.setLongAnimate = function() {
         var scalePerStep = this.calculateEnlargeStepLongAnimation();
-        var increase;
+        var increase = this._increase;
         var minScale = this._minScale;
         var scaleSum = this._scaleSum;
         var scale;
@@ -36,14 +38,15 @@ define(['../DataSources/CallbackProperty','../Core/EasingFunction','../Core/defa
             scaleSum += scalePerStep;
         }
 
-        scale = this.increase ? EasingFunction.BACK_OUT(scaleSum) : EasingFunction.BACK_IN(scaleSum);
+        scale = increase ? EasingFunction.BACK_OUT(scaleSum) : EasingFunction.BACK_IN(scaleSum);
         scale += minScale;
         return scale;
-    }, false)
+        }, false);
+
+        this._increase = increase;
     }
 
-
-    EntityHighlight.prototype.setShorAnimate = function() {
+    EntityHighlight.prototype.setShortAnimate = function() {
         var scalePerStep = this.calculateEnlargeStepShortAnimation();
         var scaleSum;
         var easingFunction = this._easingFunction;
@@ -63,6 +66,41 @@ define(['../DataSources/CallbackProperty','../Core/EasingFunction','../Core/defa
         }
     }
 
+    EntityHighlight.prototype.longAnimationStop = function() {
+        var scalePercent = this._scalePercent + 1;
+        var scaleMax = this._minScale * scalePercent;
+        var scalePerStep = this.calculateEnlargeStepLongAnimation();
+        var interval = setInterval(function() {
+            if (scaleMax <= this._scale && this._increase) {
+                this._increase = !this._increase;
+            }
+            if (this._minScale >= this.scale && !this._increase) {
+                clearInterval(interval);
+            }
+
+            this.scale += this.increase ? scalePerStep : -scalePerStep;
+            EntityHighlight.prototype.scope.billboard.scale = this._scale;
+        }, this._timeoutInterval);
+    }
+
+    EntityHighlight.prototype.shortAnimationStop = function() {
+        EntityHighlight.prototype.scope.billboard.scale = 1;
+    }
+
+    EntityHighlight.prototype.stop = function(){
+        switch (this._animateType) {
+            case 'enlarge':
+                this.longAnimationStop();
+                break;
+            case 'indication':
+                this.shortAnimationStop();
+                break;
+            default:
+                this.longAnimationStop();
+                break;
+        }
+    }
+
     EntityHighlight.prototype.calculateEnlargeStepLongAnimation = function() {
         var durationInSeconds = 3000;
         var numberOfSteps = (durationInSeconds / 2) / 16;
@@ -72,6 +110,7 @@ define(['../DataSources/CallbackProperty','../Core/EasingFunction','../Core/defa
         var scalePerStep = scaleDelta / numberOfSteps;
         return scalePerStep;
     };
+
     EntityHighlight.prototype.calculateEnlargeStepShortAnimation = function() {
         var durationInSeconds = this._speed;
         var numberOfSteps = (durationInSeconds / 2) / this._timeoutInterval;
@@ -81,7 +120,8 @@ define(['../DataSources/CallbackProperty','../Core/EasingFunction','../Core/defa
         var scalePerStep = scaleDelta / numberOfSteps;
         return scalePerStep;
     }
-    EntityHighlight.prototype.setAnimate = function() {
+
+    EntityHighlight.prototype.start = function() {
         switch (this._animateType) {
             case 'enlarge':
                 this.setLongAnimate();
@@ -93,6 +133,8 @@ define(['../DataSources/CallbackProperty','../Core/EasingFunction','../Core/defa
                 this.setLongAnimate();
                 break;
         }
+        if(!this._interval || this._indicationOnly){ this._stop() }
     }
+
     return EntityHighlight;
 });
